87c08f9328aeac043e7d3f1e2b0f3c6fafb52427
afaq hussain <138759199+afaq000@users.noreply.github.com>
2023-10-01 02:46:04 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const cors =require("cors")
 
 const app=express();
 app.use(express.json())
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST":"GET"],
+        credentials:true
+    }
+));
 const port=5000;
 app.use('/api', require("./Routes/CreateUser"))
 app.use('/api', require("./Routes/DisplayData"))
